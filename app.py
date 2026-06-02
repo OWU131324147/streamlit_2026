dd174accfb4db926d9503ca12cc3d667fefaf5b8
OWU131324147 <s1324147@cst.otsuma.ac.jp>
2026-06-02 10:16:13 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+if "kibun_history" not in st.session_state:
+    st.session_state["kibun_history"] = []
+
+col1, col2, col3, col4= st.columns(4)
+
+with col1:
+    if st.button("😊嬉しい"):
+        st.session_state["kibun_history"].append("😊嬉しい")
+
+with col2:
+    if st.button("😍楽しい"):
+        st.session_state["kibun_history"].append("😍楽しい")
+
+with col3:
+    if st.button("😪眠い"):
+        st.session_state["kibun_history"].append("😪眠い")
+
+with col4:
+    if st.button("🍕お腹すいた"):
+        st.session_state["kibun_history"].append("🍕お腹すいた")
+
+for kibun in st.session_state["kibun_history"]:
+    st.write(kibun)
